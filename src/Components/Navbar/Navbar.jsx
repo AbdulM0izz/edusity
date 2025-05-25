1da78dbab949bb9c82d843f975aa9df6fd1381c8
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-05-26 01:54:24 +0500

    Media queries for different screen size

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import Logo from '../../assets/logo.png'
 import { Link } from 'react-scroll'
-
+import Menu_icon from '../../assets/menu-icon.png'
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
 
@@ -11,11 +11,16 @@ const Navbar = () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
   },[])
+  const [mobilemenu, setmobilemenu] = useState(false);
+
+  const toggleMenu = () => {
+    mobilemenu ? setmobilemenu(false) : setmobilemenu(true);
+  }
   
   return (
     <nav className={`container ${sticky? 'dark-nav':''}`}> 
         <img src={Logo} alt="" className='logo' />
-        <ul>
+        <ul className={mobilemenu ? '' : 'hide-mobile-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
@@ -23,6 +28,7 @@ const Navbar = () => {
             <li><Link to='testiominal' smooth={true} offset={-260} duration={500}>Testimonial</Link></li>
             <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
         </ul>
+        <img src={Menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
